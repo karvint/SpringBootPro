c1770e5e16905436a06d5ad758456ea72ec627c6
karvint <875648435@qq.com>
2018-09-28 16:40:47 +0800

    --add transactional --tantianyi

--- src/main/java/com/ty/show/SchedulerApplication.java
+++ src/main/java/com/ty/show/SchedulerApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableAutoConfiguration
 @MapperScan(basePackages = "com.ty.show.dataobject.mapper")
 @EnableAsync
+@EnableTransactionManagement
 public class SchedulerApplication {
 
 	public static void main(String[] args) {
